3f0b913a98d360aefbfa06f9d022286925176867
Schubert <Tim Schubert tim.schubert@dlr.de>
2023-02-10 15:49:58 +0100

    fix range of process name

--- src/apex/process.rs
+++ src/apex/process.rs
@@ -11,7 +11,7 @@ impl ApexProcessP4 for XngHypervisor {
     ) -> Result<ProcessId, ErrorReturnCode> {
         let mut return_code = MaybeUninit::uninit();
         let mut process_id = MaybeUninit::uninit();
-        if attributes.name[31..=32] != [0, 0] {
+        if attributes.name[31..32] != [0, 0] {
             return Err(ErrorReturnCode::InvalidParam);
         }
         let mut name = [0 as cty::c_char; 30];
